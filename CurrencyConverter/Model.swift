a828acf608bce2370ac4c0a41e76e34efc5b57df
Arseny Neustroev <neustroev.arseny@gmail.com>
2019-10-25 15:31:01 +0500

    Added model file

---
+++ CurrencyConverter/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  CurrencyConverter
+//
+//  Created by Arseny Neustroev on 25/10/2019.
+//  Copyright © 2019 Arseny Neustroev. All rights reserved.
+//
+
+import UIKit
+
+class Model: NSObject {
+
+}
