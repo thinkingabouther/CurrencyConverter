a96016fd1bb39768c9d4474b5b8ad045fc44d199
Arseny Neustroev <neustroev.arseny@gmail.com>
2019-10-25 15:32:40 +0500

    Initial commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Arseny Neustroev on 25/10/2019.
+//  Copyright © 2019 Arseny Neustroev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
