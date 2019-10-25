317e9ce172bea3156bdb2a32584d29d2e081f0dd
Arseny Neustroev <neustroev.arseny@gmail.com>
2019-10-25 15:46:05 +0500

    Implemented model logic

--- CurrencyConverter/Model.swift
+++ CurrencyConverter/Model.swift
@@ -8,6 +8,46 @@
 
 import UIKit
 
-class Model: NSObject {
 
+class Model: NSObject {
+    static let sharedInstance = Model()
+    
+    var currencies : [Currency] = []
+    
+    var pathToXML: String {
+        return ""
+    }
+    
+    var urlToXML : URL?{
+        return nil
+    }
+    
+    
+    // function to load XML data from cbr.ru and saving it
+    func loadXML(date: Data) {
+        
+    }
+    
+    
+    /*
+    function to parse currencies out of XML file and load them to currencies : [Currency]
+    and make notification to main view about it
+    */
+    func parseXML() {
+        
+    }
+}
+
+class Currency{
+    var NumCode : String?
+    var CharCode : String?
+    var Name : String?
+    
+    var Value : String?
+    var valueDouble : Double?
+    
+    var Nominal : String?
+    var nominalDouble : Double?
+    
+    
 }
